cf4a0907b4d6e3b57b96c55be793fba5bf55de5a
ElizabethGosaaa <andii.candii10@gmail.com>
2026-07-02 20:47:53 -0600

    Agregué la función de cambio de color aleatorio

---
+++ script.js
@@ -0,0 +1,6 @@
+const colores = ["green", "blue", "red"];
+
+function cambiarColor(elemento) {
+    const numeroAleatorio = Math.floor(Math.random() * colores.length);
+    elemento.style.color = colores[numeroAleatorio];
+}
